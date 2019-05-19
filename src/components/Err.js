acac6d6d5997e859b71b4d6b28f4ac775d3ed9c2
Melisa Quesada <melisa.quc@gmail.com>
2019-05-18 20:16:24 -0500

    Router ready

---
+++ src/components/Err.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Err = () => {
+    return (
+        <div>
+            <p>Error: path does not exist </p>
+        </div>
+    );
+};
+
+export default Err;
---
+++ src/components/Kitchen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Kitchen = () => {
+    return (
+        <div>
+            <p>Kitchen</p>
+        </div>
+    );
+};
+
+export default Kitchen;
