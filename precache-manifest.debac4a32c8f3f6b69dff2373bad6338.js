7b8533001a26be8e0eb7d556a1dbbcdd050f6a63
Melisa Quesada <melisa.quc@gmail.com>
2019-05-28 09:22:52 -0500

    Updates

---
+++ precache-manifest.debac4a32c8f3f6b69dff2373bad6338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcaf861885c7577308fd680758b30201",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "793ae838e0ecbe62030c",
+    "url": "/GDL002-burger-queen/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "001cfcc40668507358c7",
+    "url": "/GDL002-burger-queen/static/js/2.3d3e9e38.chunk.js"
+  },
+  {
+    "revision": "793ae838e0ecbe62030c",
+    "url": "/GDL002-burger-queen/static/js/main.c86ebc79.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  }
+]);
